eb1c177e3ecfd96e41bdfc1d7184e4b08d140c69
Itamar <itamar.t.g@gmail.com>
2017-01-09 01:05:03 +0200

    Shukile

--- server.js
+++ server.js
@@ -1,9 +1,46 @@
-var app = require('express')();
-app.use(require('express').static('dist'));
+var express      = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var Users = {
+  userList : {
+    'Shuki': {username : 'Shuki',
+     password : '1234',
+     permission : 'Admin'},
+     'Itamar': {username : 'Itamar',
+     password : 'qwe123',
+     permission : 'User'}
+  },
+}
+app.use(express.static('dist'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+function requireRole(role) {
+    return function(req, res, next) {
+      var a = false;
+        for (i in role){
+          if((req.body.username || req.cookies.username) &&
+             ((Users.userList[req.body.username] || req.cookies).permission === role[i])) {
+            next();
+            a = true;
+          }
+        }
+        if(!a)
+            res.send(403);
+    }
+}
+
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + "/dist/index.html");
+  console.log('Cookies: ', req.cookies)
+  res.sendFile(__dirname + "/dist/enter.html");
 });
 
+app.post('/path', requireRole(['Admin','User']), function(req, res){
+    res.sendFile(__dirname + "/dist/index.html");
+  }
+);
 var port = process.env.PORT || 8080;
 
 app.listen(port, function() {
