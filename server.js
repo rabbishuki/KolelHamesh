a4c49e926002aa5e5fbb985d8407e2094df2d5e1
Itamar <itamar.t.g@gmail.com>
2017-01-10 00:46:32 +0200

    Fixing Visual Code Request
    הוספתי גישה סטטית לכל סוג קובץ בדיסט.

--- server.js
+++ server.js
@@ -2,17 +2,24 @@ var express      = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
+var Admin = 'Admin';
+var User = 'User';
+
 var Users = {
   userList : {
     'Shuki': {username : 'Shuki',
      password : '1234',
-     permission : 'Admin'},
+     permission : Admin},
      'Itamar': {username : 'Itamar',
      password : 'qwe123',
-     permission : 'User'}
+     permission : User}
   },
 }
 app.use(express.static('dist'));
+app.use('/javascripts', express.static(__dirname + '/dist/js'));
+app.use('/images', express.static(__dirname + '/dist/img'));
+app.use('/lib', express.static(__dirname + '/dist/lib'));
+app.use('/templates', express.static(__dirname + '/dist/templates'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
@@ -33,16 +40,28 @@ function requireRole(role) {
 }
 
 app.get('/', function(req, res) {
-  console.log('Cookies: ', req.cookies)
+  console.log('enter');
   res.sendFile(__dirname + "/dist/enter.html");
 });
 
-app.post('/path', requireRole(['Admin','User']), function(req, res){
-    res.sendFile(__dirname + "/dist/index.html");
+app.post('/login', requireRole([Admin,User]), function(req, res){
+    res.send({username : req.body.username, permission : Users.userList[req.body.username].permission});
+    
   }
 );
+app.get('/' + Admin, requireRole([Admin]), function(req, res){
+  console.log(Admin);
+  res.sendFile(__dirname + '/dist/index.html');
+});
+
+app.get('/' + User, requireRole([User]), function(req, res){
+  console.log(User);
+  res.sendFile(__dirname + '/dist/index.html');
+});
+
+
 var port = process.env.PORT || 8080;
 
 app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
+    console.log('Our app is running on http://localhost:' + port + " right now");
 });
